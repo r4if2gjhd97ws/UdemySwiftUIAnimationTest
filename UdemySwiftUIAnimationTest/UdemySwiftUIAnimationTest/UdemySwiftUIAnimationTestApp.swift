9bf887afbdbc480f6a21734141eba6d105168404
yohei <>
2024-09-14 20:22:15 +0900

    add project

---
+++ UdemySwiftUIAnimationTest/UdemySwiftUIAnimationTest/UdemySwiftUIAnimationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdemySwiftUIAnimationTestApp.swift
+//  UdemySwiftUIAnimationTest
+//
+//  Created by yohei on 2024/09/14.
+//
+
+import SwiftUI
+
+@main
+struct UdemySwiftUIAnimationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
